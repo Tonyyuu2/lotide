b6adc760524af50c5cec414d3df375172d36efc2
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-03-09 05:22:33 +0000

    updated eqArrays and asserArraysEqual

--- eqArrays.js
+++ eqArrays.js
@@ -7,15 +7,15 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function (array1, array2){
+  if (array1.length !== array2.length){
+    return false;
+  }
   for (let i = 0; i < array1.length; i++){
-    for(let y = 0; y < array2.length; y++){
-      if (array1[i] === array2[y]){
-        return true
-      } else {
-        return false
-      }
+    if (array1[i] !== array2[i]){
+      return false
     }
   }
+  return true
 }
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, '3'], [1, 2, 3]), false);
