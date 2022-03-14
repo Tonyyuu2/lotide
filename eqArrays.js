b9a168a1676d2354b3fddbd2470dd778952ca117
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-03-14 21:16:45 +0000

    exported module to test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function (array1, array2){
   if (array1.length !== array2.length){
@@ -18,7 +12,9 @@ const eqArrays = function (array1, array2){
   return true
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, '3'], [1, 2, 3]), false);
-assertEqual(eqArrays([1, 'hello', 3], [1, 2, 3]), false);
-assertEqual(eqArrays([3, 2, 3], [1, 2, 3]), false);
+module.exports = eqArrays;
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, '3'], [1, 2, 3]), false);
+// assertEqual(eqArrays([1, 'hello', 3], [1, 2, 3]), false);
+// assertEqual(eqArrays([3, 2, 3], [1, 2, 3]), false);
